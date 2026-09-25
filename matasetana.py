e497d927496e775bab87d645db179ea38aaae5fa
YJSNPI114514 <haruto.miura8@gmail.com>
2026-09-25 19:54:26 +0900

    game over tuika

--- matasetana.py
+++ matasetana.py
@@ -1,5 +1,4 @@
 import pyxel
-pyxel.init(160, 160)
 
 class App:
     def __init__(self):
@@ -8,9 +7,14 @@ class App:
 
         self.frame_count = 0
 
-        self.direction = 0
+        self.angle = 0
+        self.game_over = False
+        self.screen_size = 200
+        self.speed = self.screen_size / 10
+        pyxel.init(self.screen_size, self.screen_size)
 
-        """ self.direction
+
+        """ self.angle
         0=right
         90=down
         180=left
@@ -26,24 +30,30 @@ class App:
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_DOWN):
-            self.direction = 90
+            self.angle = 90
         if pyxel.btnp(pyxel.KEY_LEFT):
-            self.direction = 180
+            self.angle = 180
         if pyxel.btnp(pyxel.KEY_UP):
-            self.direction = 270
+            self.angle = 270
         if pyxel.btnp(pyxel.KEY_RIGHT):
-            self.direction = 0
-        print(self.direction)
+            self.angle = 0
         self.frame_count += 1
         if self.frame_count % 30 == 0:
-            if self.direction == 0:
-                self.position_x += 16
-            if self.direction == 90:
-                self.position_y += 16
-            if self.direction == 180:
-                self.position_x -= 16
-            if self.direction == 270:
-                self.position_y -= 16
+            if self.angle == 0:
+                self.position_x += self.speed
+            if self.angle == 90:
+                self.position_y += self.speed
+            if self.angle == 180:
+                self.position_x -= self.speed
+            if self.angle == 270:
+                self.position_y -= self.speed
+
+            #gameover hantei
+        if self.position_x >= self.screen_size or self.position_x < 0 or self.position_y >= self.screen_size or self.position_y < 0:
+            self.game_over = True
+        else:
+            self.game_over = False
+
 
     def draw(self):
         pyxel.cls(0)
@@ -56,8 +66,11 @@ class App:
             16,
             16,
             0,
-            rotate=self.direction
+            rotate=self.angle
         )
+
+        if self.game_over == True:
+            pyxel.text(10, 10, "GAME OVER", 12)
         # 画像を回転させたい場合：rotate 画像を拡大したい場合：scale の引数を追加できる
 
 App()
